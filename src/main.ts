4280950bd42d937cf7932ee479a6280acd53cd70
hchangjae <hcj@payw.org>
2022-08-26 04:44:23 +0900

    충돌 처리

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {init, GameLoop, Sprite} from 'kontra';
+import {init, GameLoop, Sprite, collides} from 'kontra';
 
 const {canvas} = init();
 
@@ -9,6 +9,7 @@ type EnemyTrait = {
   y?: number;
   color?: string;
 };
+
 const enemyFactory = ({scale, speed, x = 256, y = 160, color = 'red'}: EnemyTrait) => {
   return Sprite({
     width: scale,
@@ -20,37 +21,56 @@ const enemyFactory = ({scale, speed, x = 256, y = 160, color = 'red'}: EnemyTrai
   });
 };
 
-const arrayFactory = (length: number) => Array.from({length});
+const smallEnemies:Sprite[] = []
 
-const smallEnemies = arrayFactory(50).map(() =>
-  enemyFactory({
-    scale: 2,
-    speed: -2 * Math.random(),
-  })
-);
+const middleEnemies:Sprite[] = []
 
-const middleEnemies = arrayFactory(20).map(() =>
-  enemyFactory({
-    scale: 4,
-    speed: -4 * Math.random(),
-    y: 150,
-    color: 'green',
-  })
-)
+const getAllEnemies = () => {
+  const small = smallEnemies
+  const middle = middleEnemies
+  return [...small, ...middle]
+};
 
-const allEnemies = [...smallEnemies, ...middleEnemies];
+setInterval(() => {
+  if(smallEnemies.length < 50){
+    smallEnemies.push(enemyFactory({
+      scale: 2,
+      speed: 2 * Math.random(),
+      x: 0,
+    }))
+  }
+  if(smallEnemies.length < 50){
+    middleEnemies.push(enemyFactory({
+      scale: 4,
+      speed: -4 * Math.random(),
+      color: 'green',
+    }))
+  }
+}, 100)
 
 const loop = GameLoop({
   update: () => {
-    allEnemies.forEach(enemy => {
-      if (enemy.x <= 0 || enemy.x > canvas.width) {
+    getAllEnemies().forEach(enemy => {
+      if (enemy.x < 0 || enemy.x > canvas.width) {
         enemy.dx = -enemy.dx;
       }
       enemy.update();
     });
+
+    // 충돌 확인
+    smallEnemies.forEach(s => {
+      middleEnemies.forEach(m => {
+        if(collides(s, m)){
+          const sIndex = smallEnemies.indexOf(s)
+          const mIndex = middleEnemies.indexOf(m)
+          smallEnemies.splice(sIndex, 1)
+          middleEnemies.splice(mIndex, 1)
+        }
+      })
+    })
   },
   render: () => {
-    allEnemies.forEach(enemy => enemy.render());
+    getAllEnemies().forEach(enemy => enemy.render());
   },
 });
 
