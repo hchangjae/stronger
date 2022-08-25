fedbde4bbbcdad1bc88e4a3edc7eff839e33d032
Jack2ee-dev <jack2ee.dev@gmail.com>
2022-08-25 16:52:36 +0900

    feat: implement basic enemy

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,57 @@
-export default {};
+import {init, GameLoop, Sprite} from 'kontra';
+
+const {canvas} = init();
+
+type EnemyTrait = {
+  scale: number;
+  speed: number;
+  x?: number;
+  y?: number;
+  color?: string;
+};
+const enemyFactory = ({scale, speed, x = 256, y = 160, color = 'red'}: EnemyTrait) => {
+  return Sprite({
+    width: scale,
+    height: scale,
+    dx: speed,
+    x,
+    y,
+    color,
+  });
+};
+
+const arrayFactory = (length: number) => Array.from({length});
+
+const smallEnemies = arrayFactory(50).map(() =>
+  enemyFactory({
+    scale: 2,
+    speed: -2 * Math.random(),
+  })
+);
+
+const middleEnemies = arrayFactory(20).map(() =>
+  enemyFactory({
+    scale: 4,
+    speed: -4 * Math.random(),
+    y: 150,
+    color: 'green',
+  })
+)
+
+const allEnemies = [...smallEnemies, ...middleEnemies];
+
+const loop = GameLoop({
+  update: () => {
+    allEnemies.forEach(enemy => {
+      if (enemy.x <= 0 || enemy.x > canvas.width) {
+        enemy.dx = -enemy.dx;
+      }
+      enemy.update();
+    });
+  },
+  render: () => {
+    allEnemies.forEach(enemy => enemy.render());
+  },
+});
+
+loop.start();
---
+++ src/unit/enemy.ts
@@ -0,0 +1,12 @@
+import {Sprite} from 'kontra';
+
+const enemy = Sprite({
+  x: 200,
+  y: 100,
+  width: 2,
+  height: 2,
+  dx: -2,
+  color: 'red',
+})
+
+export default enemy;
