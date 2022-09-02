7cbec5e0069027cb643ab44b30917234a59e0ac0
Jack2ee-dev <jack2ee.dev@gmail.com>
2022-09-02 15:28:55 +0900

    feat: implement game info by dom

---
+++ src/controller/Info.ts
@@ -0,0 +1,19 @@
+const $ = (selector: string) => document.querySelector(selector);
+const update = (
+  $el: Element | null,
+  value: string,
+  template: (value: string) => string
+) => {
+  if ($el) {
+    $el.textContent = template(value);
+  }
+};
+
+export const updateResource = (value: number) =>
+  update($('.resource'), `${value}`, (value) => `👻 ${value}`);
+
+export const updateWave = (value: number) =>
+  update($('.wave'), `${value}`, (value) => `Wave ${value}`);
+
+export const updateGeneration = (value: number) =>
+  update($('.generation'), `${value}`, (value) => `(Generation ${value})`);
