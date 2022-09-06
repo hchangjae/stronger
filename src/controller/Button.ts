10cffabe901c716568471e828a4f19e5043320f7
Junsik Shim <junsik.shim@gmail.com>
2022-09-06 17:33:01 +0900

    버튼 수정

--- src/controller/Button.ts
+++ src/controller/Button.ts
@@ -4,7 +4,7 @@ import { WeaponUpgradeType } from '../data/upgrade/weapons';
 
 export const createButton = (text: string, className: string, onClick: () => void): HTMLButtonElement => {
   const button = document.createElement('button');
-  button.textContent = text;
+  button.innerHTML = text;
   button.classList.add(className);
   button.addEventListener('click', onClick);
   return button;
@@ -16,7 +16,11 @@ const appendButton = ($target: Element | null, $button: HTMLButtonElement) => {
 
 const $upgradeWeapon = $('.upgrades .weapons');
 const createWeaponButton = (upgrade: WeaponUpgradeType, onClick: () => void) =>
-  createButton(`${upgrade.label} (👻${upgrade.resourceNeeded})`, 'button', onClick);
+  createButton(
+    `<div>${upgrade.label} <span class="resource">👻 ${upgrade.resourceNeeded}</span></div>`,
+    'button',
+    onClick
+  );
 
 export const appendUpgradeWeapon = (upgrade: WeaponUpgradeType, onClick: () => void) => {
   appendButton($upgradeWeapon, createWeaponButton(upgrade, onClick));
@@ -24,7 +28,11 @@ export const appendUpgradeWeapon = (upgrade: WeaponUpgradeType, onClick: () => v
 
 const $upgradePassive = $('.upgrades .passives');
 const createUpgradeButton = (upgrade: Upgrade, onClick: () => void) =>
-  createButton(`${upgrade.getTarget()} +${upgrade.getAmount()} (👻${upgrade.getResourceNeeded()})`, 'button', onClick);
+  createButton(
+    `<div>${upgrade.getTarget()} <span class="amount">+${upgrade.getAmount()}</span> <span class="resource">👻 ${upgrade.getResourceNeeded()}</span></div>`,
+    'button',
+    onClick
+  );
 
 export const appendUpgradePassive = (upgrade: Upgrade, onClick: () => void) => {
   appendButton($upgradePassive, createUpgradeButton(upgrade, onClick));
