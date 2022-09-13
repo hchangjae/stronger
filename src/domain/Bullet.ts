28e392c4ad03fdc368c1d53e2ac10938b8d46d01
Jack2ee-dev <jack2ee.dev@gmail.com>
2022-09-13 16:10:24 +0900

    feat: remove getter & setter in Bullet.ts

--- src/domain/Bullet.ts
+++ src/domain/Bullet.ts
@@ -1,5 +1,4 @@
-import { GameObject, GameObjectClass, collides, Sprite, imageAssets } from 'kontra';
-import { TOWER_POSITION } from '../main';
+import { GameObject, GameObjectClass } from 'kontra';
 
 type BulletProps = {
   x: number;
@@ -36,42 +35,6 @@ class Bullet extends GameObjectClass {
     this.sR = sR;
     this.target = target;
     this.isCollided = false;
-  }
-
-  setPos(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-
-  getIsCollides() {
-    return collides(this, this.target);
-  }
-
-  setTtl(value: number) {
-    this.ttl = value;
-  }
-
-  getDistanceFromTarget() {
-    return Math.sqrt(Math.pow(this.target.x - this.x, 2) + Math.pow(this.target.y - this.y, 2));
-  }
-
-  setRotation(value: number) {
-    this.rotation = value;
-  }
-
-  setIsCollided(value: boolean) {
-    this.isCollided = value;
-  }
-
-  render() {
-    Sprite({
-      image: imageAssets[this.image],
-      x: TOWER_POSITION + 40,
-      y: 170,
-      width: 20,
-      height: 33,
-      anchor: { x: 0.5, y: 0.5 },
-    }).render();
   }
 }
 
